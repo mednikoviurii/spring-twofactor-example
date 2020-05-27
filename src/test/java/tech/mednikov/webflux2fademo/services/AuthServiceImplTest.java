d08e83c04164b55fae8a8a62a502dac1cdce0ec3
Iurii Mednikov <yuri.mednikov@mednikov.net>
2020-05-27 16:28:36 +0200

    Modified AuthServiceImplTest

--- src/test/java/tech/mednikov/webflux2fademo/services/AuthServiceImplTest.java
+++ src/test/java/tech/mednikov/webflux2fademo/services/AuthServiceImplTest.java
@@ -10,6 +10,8 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
+import tech.mednikov.webflux2fademo.errors.AlreadyExistsException;
+import tech.mednikov.webflux2fademo.errors.LoginDeniedException;
 import tech.mednikov.webflux2fademo.managers.TokenManager;
 import tech.mednikov.webflux2fademo.managers.TotpManager;
 import tech.mednikov.webflux2fademo.models.LoginRequest;
@@ -57,53 +59,61 @@ class AuthServiceImplTest {
 
     @Test
     void loginDeniedTest(){
+        final String email = "john.doe@mail.com";
+
         LoginRequest request = new LoginRequest();
-        request.setEmail("john.doe@mail.com");
-        request.setPassword("secret");
-        request.setCode("123456");
+        request.setEmail(email);
 
-        Mockito.when(repository.findByEmail("john.doe@mail.com")).thenReturn(Mono.empty());
+        Mockito.when(repository.findByEmail(email)).thenReturn(Mono.empty());
 
-        StepVerifier.create(service.login(request)).expectError();
+        StepVerifier.create(service.login(request)).expectError(LoginDeniedException.class);
     }
 
     @Test
     void signupSuccessTest(){
+        final String email = "john.doe@mail.com";
+        final String secretKey = "secretkey";
+        final String userId = "userId";
+        final String token = "token";
+
         SignupRequest request = new SignupRequest();
-        request.setEmail("john.doe@mail.com");
+        request.setEmail(email);
         request.setPassword("secret");
 
         User user = new User();
-        user.setEmail("john.doe@mail.com");
-        user.setSecretKey("secretkey");
-        user.setUserId("userId");
+        user.setEmail(email);
+        user.setSecretKey(secretKey);
+        user.setUserId(userId);
 
-        Mockito.when(repository.findByEmail("john.doe@mail.com")).thenReturn(Mono.empty());
-        Mockito.when(totpManager.generateSecret()).thenReturn("secretkey");
-        Mockito.when(tokenManager.issueToken(Mockito.anyString())).thenReturn("token");
-        Mockito.when(repository.save(Mockito.any(User.class))).thenReturn(Mono.just(user));
+        Mono<User> userSource = Mono.just(user);
+
+        Mockito.when(repository.findByEmail(email)).thenReturn(Mono.empty());
+        Mockito.when(totpManager.generateSecret()).thenReturn(secretKey);
+        Mockito.when(tokenManager.issueToken(Mockito.anyString())).thenReturn(token);
+        Mockito.when(repository.save(Mockito.any(User.class))).thenReturn(userSource);
 
         StepVerifier.create(service.signup(request))
                 .assertNext(result -> Assertions.assertThat(result)
-                        .hasFieldOrPropertyWithValue("userId", "userId")
-                        .hasFieldOrPropertyWithValue("token", "token")
-                        .hasFieldOrPropertyWithValue("secretKey", "secretkey"))
+                        .hasFieldOrPropertyWithValue("userId", userId)
+                        .hasFieldOrPropertyWithValue("token", token)
+                        .hasFieldOrPropertyWithValue("secretKey", secretKey))
                 .verifyComplete();
     }
 
     @Test
     void signupDeniedTest(){
+        final String email = "john.doe@mail.com";
+
         SignupRequest request = new SignupRequest();
-        request.setEmail("john.doe@mail.com");
-        request.setPassword("secret");
+        request.setEmail(email);
 
         User user = new User();
-        user.setEmail("john.doe@mail.com");
-        user.setSecretKey("secretkey");
-        user.setUserId("userId");
+        user.setEmail(email);
 
-        Mockito.when(repository.findByEmail("john.doe@mail.com")).thenReturn(Mono.just(user));
+        Mono<User> userSource = Mono.just(user);
 
-        StepVerifier.create(service.signup(request)).expectError();
+        Mockito.when(repository.findByEmail(email)).thenReturn(userSource);
+
+        StepVerifier.create(service.signup(request)).expectError(AlreadyExistsException.class);
     }
 }
